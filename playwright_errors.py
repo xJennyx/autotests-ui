d7db5f48cb49294dc11c6d8eb6876d7ea94dad35
Viktor <xjennyx068@gmail.com>
2025-11-06 10:26:34 +0300

    Работа с авторизацией Playwright

--- playwright_errors.py
+++ playwright_errors.py
@@ -1,6 +1,5 @@
 from playwright.sync_api import sync_playwright, expect
 
-
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
     page = browser.new_page()
--- playwright_registration.py
+++ playwright_registration.py
@@ -1,23 +1,31 @@
 from playwright.sync_api import sync_playwright, expect
 
-
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
+    context = browser.new_context()
+    page = context.new_page()
 
     page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
 
-    email_registration_input = page.get_by_test_id('registration-form-email-input').locator('input')
-    username_registration_input = page.get_by_test_id('registration-form-username-input').locator('input')
-    password_registration_input = page.get_by_test_id('registration-form-password-input').locator('input')
-    button_registration = page.get_by_test_id('registration-page-registration-button')
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
 
-    email_registration_input.fill('user.name@gmail.com')
-    username_registration_input.fill('username')
-    password_registration_input.fill('password')
-    button_registration.click()
+    email_input.fill('user.name@gmail.com')
+    username_input.fill('username')
+    password_input.fill('password')
 
-    dashboard_header = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard_header).to_have_text('Dashboard')
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
 
-    print('Test successful')
+    context.storage_state(path='browser-state.json')
+
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state='browser-state.json')
+    page = context.new_page()
+
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
+
+    page.wait_for_timeout(5000)
