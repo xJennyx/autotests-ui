e34ca85bef80f460b4087f0af078fa338107fa51
Viktor <xjennyx068@gmail.com>
2025-11-11 09:45:54 +0300

    Введение в Pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,17 @@
+def test_user_login():
+    print("Hello!")
+
+
+class TestUserLogin:
+    def test_1(self):
+        ...
+
+    def test_2(self):
+        ...
+
+
+def test_assert_positive_case():
+    assert (2 + 2) == 4
+
+def test_assert_negative_case():
+    assert (2 + 2) == 5, "(2 + 2) != 5"
