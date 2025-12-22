ecf094316bb67471247e8763cbdbe2b41d819ffd
Viktor <xjennyx068@gmail.com>
2025-12-22 11:10:05 +0300

    Практикуемся в реализации PageObject

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -8,7 +8,7 @@ from pages.registration_page import RegistrationPage
 @pytest.mark.registration
 def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
         registration_page.visit(
-                'https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration'        )
+                'https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
         registration_page.fill_registration_form(email='user.name@gmail.com', username='username', password='password')
         registration_page.click_registration_button()
-        dashboard_page.check_dashboard_text_title(text_title='Dashboard')
+        dashboard_page.check_visible_dashboard_title(text_title='Dashboard')
