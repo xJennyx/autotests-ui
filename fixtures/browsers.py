f723175899f92c57d3602a3358438abb28708742
Viktor <xjennyx068@gmail.com>
2026-03-25 10:24:41 +0300

    pydantic

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,9 +1,9 @@
 import pytest
 from playwright.sync_api import Page, Playwright
-import allure
 from pages.authentication.registration_page import RegistrationPage
 from _pytest.fixtures import SubRequest
 from tools.playwright.pages import initialize_playwright_page
+from config import settings
 
 
 @pytest.fixture
@@ -20,10 +20,14 @@ def initialize_browser_state(playwright: Playwright):
 
         registration_page = RegistrationPage(page=page)
         registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-        registration_page.registration_form.fill(email='user.name@gmail.com', username='username', password='password')
+        registration_page.registration_form.fill(
+            email=settings.test_user.email,
+            username=settings.test_user.username,
+            password=settings.test_user.password
+        )
         registration_page.registration_button.click()
 
-        context.storage_state(path='browser-state.json')
+        context.storage_state(path=settings.browser_state_file)
         browser.close()
 
 
@@ -32,5 +36,5 @@ def chromium_page_with_state(request: SubRequest, initialize_browser_state, play
     yield from initialize_playwright_page(
         playwright,
         test_name=request.node.name,
-        storage_state='browser-state.json'
+        storage_state=settings.browser_state_file
     )
