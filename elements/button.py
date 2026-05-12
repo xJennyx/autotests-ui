242d00798c23beaf3bfdc13f7d1deadcf2782142
Viktor <xjennyx068@gmail.com>
2026-05-12 10:34:18 +0300

    Добавление информации о переменных окружения

--- elements/button.py
+++ elements/button.py
@@ -3,6 +3,10 @@ import allure
 from elements.base_element import BaseElement
 from playwright.sync_api import expect
 
+from tools.logger import get_logger
+
+logger = get_logger("BUTTON_ELEMENT")
+
 
 class Button(BaseElement):
     @property
@@ -10,11 +14,15 @@ class Button(BaseElement):
         return "button"
 
     def check_enabled(self, nth: int = 0, **kwargs):
-        with allure.step(f'Checking that {self.type_of} "{self.name}" is enabled'):
+        step = f'Checking that {self.type_of} "{self.name}" is enabled'
+        with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
+            logger.info(step)
             expect(locator).to_be_enabled()
 
     def check_disabled(self, nth: int = 0, **kwargs):
-        with allure.step(f'Checking that {self.type_of} "{self.name}" is disabled'):
+        step = f'Checking that {self.type_of} "{self.name}" is disabled'
+        with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
+            logger.info(step)
             expect(locator).to_be_disabled()
--- elements/input.py
+++ elements/input.py
@@ -3,6 +3,10 @@ import allure
 from elements.base_element import BaseElement
 from playwright.sync_api import expect, Locator
 
+from tools.logger import get_logger
+
+logger = get_logger("INPUT_ELEMENT")
+
 
 class Input(BaseElement):
     @property
@@ -13,11 +17,15 @@ class Input(BaseElement):
         return super().get_locator(nth, **kwargs).locator('input')
 
     def fill(self, value: str, nth: int = 0, **kwargs):
-        with allure.step(f'Fill {self.type_of} "{self.name}" to value "{value}"'):
+        step = f'Fill {self.type_of} "{self.name}" to value "{value}"'
+        with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
+            logger.info(step)
             locator.fill(value)
 
     def check_have_value(self, value: str, nth: int = 0, **kwargs):
-        with allure.step(f'Checking that {self.type_of} "{self.name}" has a value "{value}"'):
+        step = f'Checking that {self.type_of} "{self.name}" has a value "{value}"'
+        with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
+            logger.info(step)
             expect(locator).to_have_value(value)
